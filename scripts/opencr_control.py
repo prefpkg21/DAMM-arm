f077bcfd26641b8d6511cfeb308088b7b1626026
Blake <prefpkg21@users.noreply.github.com>
2020-07-15 12:27:31 -0400

    Use custom damm_msgs package

--- scripts/opencr_control.py
+++ scripts/opencr_control.py
@@ -2,7 +2,8 @@
 import rospy
 import time
 from sensor_msgs.msg import Joy
-from std_msgs.msg  import Float32MultiArray
+from std_msgs.msg  import Float32MultiArray, MultiArrayDimension
+from damm_msgs.msg import Servos, Light
 
 move_strength0 = 300
 move_strength1 = 100
@@ -39,13 +40,13 @@ class MoveArm():
     def __init__(self):
         #self.turret_aim_publisher = rospy.Publisher('/dynamixel_workbench/dynamixel_command', MX, queue_size=2)
         self.cmd_subscriber = rospy.Subscriber('/bluetooth_teleop/joy',Joy, self.handle_joystick, queue_size=2)
-	self.goal_pub = rospy.Publisher('damm_goal', Float32MultiArray, queue_size=1)
+	self.goal_pub = rospy.Publisher('damm_goal', Servos, queue_size=1)
         #self.servo_position_updater = rospy.Subscriber('/dynamixel_workbench/dynamixel_state', DynamixelStateList, self.update_current_position, queue_size=1)
         
         self.ctrl_c= False
         rospy.on_shutdown(self.shutdownhook)
 
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(30)
 	self.prev_stamp = 0
         self.dxl0_pos =  9999
 	self.dxl1_pos =  9999
@@ -53,6 +54,10 @@ class MoveArm():
         self.prev_dxl0 = 2050
         self.prev_dxl1 = 2040
         self.prev_dxl2 = 1530
+	self.dxl1_goal = 2050
+        self.dxl2_goal = 2040
+        self.dxl3_goal = 1530
+	self.linsrvo_chng = 0
 
     # def publish_once_in_dyna_command(self, cmd):
     #     while not self.ctrl_c:
@@ -71,18 +76,11 @@ class MoveArm():
 
     
     def handle_joystick(self, data):
-	stamp_diff = data.header.seq - self.prev_stamp 
-	if stamp_diff <= 5:
-		return
-
-
 
 	# Simple check for PS4 driver to set initial value
 	#   for the right and left triggers
-	if data.axes[2] == 0 and data.axes[4] == 0:
-		return
-	msg = Float32MultiArray()
-        #msg.dim.size = 4
+	
+
 	# Read joystick message values 
         dxl0_scale = data.axes[2] # right thumb horiz
 	right = data.axes[4] - 1  #right trigger
@@ -90,9 +88,24 @@ class MoveArm():
 	dxl1_scale =  left - right
 
 	dxl2_cut = data.axes[6] # normally 0 or 1/-1 pressed left/right d-pad
+	if data.axes[5] > 0.2:
+	    self.linsrvo_chng = 1  #right thumb vertical
+	elif data.axes[5] < -0.2:
+	    self.linsrvo_chng = -1
+	else:
+	    self.linsrvo_chng = 0
+	self.dxl1_goal = dxl0_scale
+        self.dxl2_goal = dxl1_scale
+        self.dxl3_goal = dxl2_cut
+	
 
-	linsrvo_chng = data.axes[5]  #right thumb vertical
-	msg.data = [dxl0_scale, dxl1_scale, dxl2_cut, linsrvo_chng]
+    def publish_goals(self):
+	
+	msg = Servos()
+	msg.dxl1 = self.dxl1_goal
+	msg.dxl2 = self.dxl2_goal
+	msg.dxl3 =  self.dxl3_goal
+	msg.linear = self.linsrvo_chng
 	self.goal_pub.publish(msg)
 
         
@@ -113,7 +126,10 @@ if __name__ == "__main__":
     #GPIO.setmode(GPIO.BOARD)
     # set pin as an output pin with optional initial state of HIGH
     #GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
-    turret = MoveArm()
+    arm_ctl = MoveArm()
 
     while not rospy.is_shutdown():
-        turret.rate.sleep
+        arm_ctl.publish_goals()
+	arm_ctl.rate.sleep()
+
+
